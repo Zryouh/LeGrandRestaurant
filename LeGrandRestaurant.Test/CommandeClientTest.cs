01dbf6a4cbba606ef63d784f613cf5338bdb0521
YZ <popoa@live.fr>
2021-12-23 20:17:50 +0100

    Scope Command

---
+++ LeGrandRestaurant.Test/CommandeClientTest.cs
@@ -0,0 +1,45 @@
+﻿using Xunit;
+
+namespace LeGrandRestaurant.Test
+{
+    public class CommandeClientTest
+    {
+        [Fact(DisplayName = "ÉTANT DONNE un serveur dans un restaurant " +
+                            "QUAND il prend une commande de nourriture " +
+                            "ALORS cette commande apparaît dans la liste de tâches de la cuisine de ce restaurant")]
+        public void CommandeClient_ajoutTache()
+        {
+            // ÉTANT DONNE un serveur dans un restaurant
+            var serveur = new Serveur();
+            var commande = new Commande();
+            var restaurant = new Restaurant(serveur);
+
+
+            // QUAND il prend une commande de nourriture
+            serveur.getFood(commande);
+
+            // ALORS cette commande apparaît dans la liste de tâches de la cuisine de ce restaurant
+            var tacheCuisine = restaurant.TacheCuisine;
+            Assert.Contains(commande, tacheCuisine);
+        }
+
+        [Fact(DisplayName = "ÉTANT DONNE un serveur dans un restaurant " +
+                            "QUAND il prend une commande de boisson " +
+                            "ALORS cette commande n'apparaît pas dans la liste de tâches de la cuisine de ce restaurant")]
+        public void CommandeClient_retireTache()
+        {
+            // ÉTANT DONNE un serveur dans un restaurant
+            var serveur = new Serveur();
+            var commande = new Commande();
+            var restaurant = new Restaurant(serveur);
+
+
+            // QUAND il prend une commande de boisson
+            serveur.getDrink(commande);
+
+            // ALORS cette commande n'apparaît pas dans la liste de tâches de la cuisine de ce restaurant
+            var tacheCuisine = restaurant.TacheCuisine;
+            Assert.DoesNotContain(commande, tacheCuisine);
+        }
+    }
+}
--- LeGrandRestaurant/Restaurant.cs
+++ LeGrandRestaurant/Restaurant.cs
@@ -1,20 +1,29 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace LeGrandRestaurant
 {
     public class Restaurant
     {
         private readonly Table[] _tables;
-
+        private readonly Serveur[] _serveurs;
+      
         public Restaurant(params Table[] tables)
         {
             _tables = tables;
         }
-        
+        public Restaurant(params Serveur[] serveurs)
+        {
+            _serveurs = serveurs;
+        }
+
         public Table[] TablesesLibres => _tables.Where(table => table.EstLibre).ToArray();
+
+        public IEnumerable<Commande> TacheCuisine => _serveurs.SelectMany(serveur => serveur.GetCommandes);
 
         public void DébuterService()
         {
+
         }
     }
 }
