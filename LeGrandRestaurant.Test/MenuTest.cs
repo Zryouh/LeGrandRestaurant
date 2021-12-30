6900b41622e8e090505ba3339c9bc3fcb74e0295
YZ <popoa@live.fr>
2021-12-30 22:10:18 +0100

    merge

--- LeGrandRestaurant.Test/MenuTest.cs
+++ LeGrandRestaurant.Test/MenuTest.cs
@@ -40,5 +40,35 @@ namespace LeGrandRestaurant.Test
             
             Assert.Equal(franchise.getPricePlatRestaurant("Pates au saumon", 1),18 );
         }
+
+        [Fact(DisplayName = "ÉTANT DONNE un restaurant appartenant à une franchise et définissant un menu ayant un plat " +
+                           "ET une franchise définissant un menu ayant le même plat " +
+                           "QUAND la franchise modifie le prix du plat " +
+                           "ALORS le prix du plat dans le menu du restaurant reste inchangé")]
+        public void NoModification_Prix_Menu()
+        {
+            // ÉTANT DONNE un restaurant appartenant à une franchise et définissant un menu ayant un plat
+            var franchise = new Franchise();
+            var table = new Table();
+            var restaurant = new Restaurant(1);
+            restaurant.Franchiser();
+            franchise.AjouterRestaurant(restaurant);
+
+            // ET une franchise définissant un menu ayant le même plat
+            var menu = new Menu();
+            var plat = new Plat("Pates au saumon", 15.2);
+
+            menu.ajouterPlat(plat);
+            franchise.AjouteMenu(menu);
+            restaurant.Menu = menu;
+            // QUAND la franchise modifie le prix du plat
+            double Nouveau_prix = 18;
+            franchise.changerPrix("Pates au saumon", Nouveau_prix);
+            franchise.changerPrixPlatRestaurant("Pates au saumon", 1, 25);
+            //ALORS le prix du plat dans le menu du restaurant reste inchangé
+            
+
+            Assert.Equal(franchise.getPricePlatRestaurant("Pates au saumon", 1), 25);
+        }
     }
 }
